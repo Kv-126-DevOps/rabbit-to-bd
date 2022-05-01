b03872101620a5165d1678b5faf235854c16a481
Olha Buchynska <olgabuchinskaya@gmail.com>
2022-05-02 00:06:49 +0300

    comment mysql records

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This application grabs data from RabbitMQ queue and saves to PostgreSQL database
     docker run --network=kv126 -d --name rabbit -e RABBITMQ_DEFAULT_USER=mquser -e RABBITMQ_DEFAULT_PASS=mqpass -p 15672:15672 rabbitmq:3.9-management
 
 ### Run rabbit-to-bd
-    git clone --branch develop https://github.com/Kv-126-DevOps/rabbit-to-bd.git /opt/rabbit-to-bd
+    git clone --branch 1-rabbit-to-bd-code-refactoring https://github.com/Kv-126-DevOps/rabbit-to-bd.git /opt/rabbit-to-bd
     docker run --network=kv126 -e POSTGRES_HOST=postgres -e POSTGRES_PORT=5432 -e POSTGRES_USER=dbuser -e POSTGRES_PW=dbpass -e POSTGRES_DB=postgres -e RABBIT_HOST=rabbit -e RABBIT_PORT=5672 -e RABBIT_USER=mquser -e RABBIT_PW=mqpass -e RABBIT_QUEUE=restapi -d --name rabbit-to-bd -v /opt/rabbit-to-bd:/app python:3.9-slim sleep infinity
     docker exec rabbit-to-bd pip install -r /app/requirements.txt
     docker exec -d rabbit-to-bd bash -c "cd /app && python ./app.py"
--- config.py
+++ config.py
@@ -20,17 +20,17 @@ POSTGRES_USER = getEnvVariable("POSTGRES_USER")
 POSTGRES_PW = getEnvVariable("POSTGRES_PW")
 POSTGRES_DB = getEnvVariable("POSTGRES_DB")
 
-MYSQL_HOST = getEnvVariable("MYSQL_HOST")
-MYSQL_PORT = getEnvVariable("MYSQL_PORT")
-MYSQL_USER = getEnvVariable("MYSQL_USER")
-MYSQL_PW = getEnvVariable("MYSQL_PW")
-MYSQL_DB = getEnvVariable("MYSQL_DB")
+#MYSQL_HOST = getEnvVariable("MYSQL_HOST")
+#MYSQL_PORT = getEnvVariable("MYSQL_PORT")
+#MYSQL_USER = getEnvVariable("MYSQL_USER")
+#MYSQL_PW = getEnvVariable("MYSQL_PW")
+#MYSQL_DB = getEnvVariable("MYSQL_DB")
 
-MSSQL_HOST = getEnvVariable("MSSQL_HOST")
-MSSQL_PORT = getEnvVariable("MSSQL_PORT")
-MSSQL_USER = getEnvVariable("MSSQL_USER")
-MSSQL_PW = getEnvVariable("MSSQL_PW")
-MSSQL_DB = getEnvVariable("MSSQL_DB")
+#MSSQL_HOST = getEnvVariable("MSSQL_HOST")
+#MSSQL_PORT = getEnvVariable("MSSQL_PORT")
+#MSSQL_USER = getEnvVariable("MSSQL_USER")
+#MSSQL_PW = getEnvVariable("MSSQL_PW")
+#MSSQL_DB = getEnvVariable("MSSQL_DB")
 
 RABBIT_HOST = getEnvVariable("RABBIT_HOST")
 RABBIT_PORT = getEnvVariable("RABBIT_PORT")
@@ -40,8 +40,8 @@ RABBIT_QUEUE = getEnvVariable("RABBIT_QUEUE")
 
 
 POSTGRES_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PW}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
-MYSQL_URL = f'mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PW}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
-MSSQL_URL = f'mssql+pymssql://{MSSQL_USER}:{MSSQL_PW}@{MSSQL_HOST}:{MSSQL_PORT}/{MSSQL_DB}'
+#MYSQL_URL = f'mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PW}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}'
+#MSSQL_URL = f'mssql+pymssql://{MSSQL_USER}:{MSSQL_PW}@{MSSQL_HOST}:{MSSQL_PORT}/{MSSQL_DB}'
 
 DB_URL = POSTGRES_URL
 
